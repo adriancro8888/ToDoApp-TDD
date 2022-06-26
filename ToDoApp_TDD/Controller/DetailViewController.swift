fe3b850b7315444bc2ed10fcf327611c69da0a9f
adriancro8888 <adriancro8888@gmail.com>
2022-06-26 18:21:31 +0300

    Created tests for DetailViewController Elements

---
+++ ToDoApp_TDD/Controller/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  ToDoApp_TDD
+//
+//  Created by Felix Titov on 6/26/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+}
---
+++ ToDoApp_TDDTests/Controller/DetailViewControllerTests.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewControllerTests.swift
+//  ToDoApp_TDDTests
+//
+//  Created by Felix Titov on 6/26/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import XCTest
+@testable import ToDoApp_TDD
+
+class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
+    
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: DetailViewController.self)) as? DetailViewController
+        
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+    
+    func testHasTitleLabel() {
+        XCTAssertNotNil(sut.titleLabel)
+        XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
+        
+    }
+    
+    func testHasDescriptionLabel() {
+        XCTAssertNotNil(sut.descriptionLabel)
+        XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDateLabel() {
+        XCTAssertNotNil(sut.dateLabel)
+        XCTAssertTrue(sut.dateLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasMapView() {
+        XCTAssertNotNil(sut.mapView)
+        XCTAssertTrue(sut.mapView.isDescendant(of: sut.view))
+    }
+}
