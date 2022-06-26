9b30c555950a4f233dae4bbfc0da6af0c1ed228f
adriancro8888 <adriancro8888@gmail.com>
2022-06-26 17:24:12 +0300

    Configured Cell tasks

--- ToDoApp_TDD/View/TaskCell.swift
+++ ToDoApp_TDD/View/TaskCell.swift
@@ -11,8 +11,20 @@ import UIKit
 
 class TaskCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     func configure(withTask task: Task) {
+        self.titleLabel.text = task.title
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy"
+        
+        if let date = task.date {
+            let dateString = dateFormatter.string(from: date)
+            dateLabel.text = dateString
+            
+        }
         
     }
 }
--- ToDoApp_TDDTests/View/TaskCellTests.swift
+++ ToDoApp_TDDTests/View/TaskCellTests.swift
@@ -37,6 +37,42 @@ class TaskCellTests: XCTestCase {
     func testCellHasTitleLabelInContentView() {
         XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
     }
+    
+    func testCellHasLocationLabel() {
+        XCTAssertNotNil(cell.locationLabel)
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasDateLabel() {
+        XCTAssertNotNil(cell.dateLabel)
+    }
+    
+    func testCellHasDateLabelInContentView() {
+        XCTAssertTrue(cell.dateLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testConfigureSetsTitile() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        XCTAssertEqual(cell.titleLabel.text, task.title)
+    }
+    
+    func testConfigureSetsDate() {
+        let task = Task(title: "Foo")
+        cell.configure(withTask: task)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy"
+        
+        let date = task.date
+        let dateString = dateFormatter.string(from: date!)
+        
+        XCTAssertEqual(cell.dateLabel.text, dateString)
+    }
 }
 
 extension TaskCellTests {
